8fdfcf7131397c08514bcf3c65809a7075cfb097
bawasaab <bawa_d@ymail.com>
2015-06-26 21:40:37 +0530

    Create example.php
    This is example code to test the same.

---
+++ example.php
@@ -0,0 +1,63 @@
+<?php
+    
+    include_once 'connection.php';
+    include_once './HelpMe.php';
+    
+    /**
+
+     * @var array Holds data as key value pair     */
+    $data = [];
+    
+    /***************************SELECT QUERY *********************************/
+    
+    /*$data[] = 'employeename';
+    $data[] = 'employee_mail_id';
+    $data[] = 'password';
+
+    $Condition = "employee_id = '698' ";
+    $res = HelpMe::SelectMe($data, 'employeemaster', $Condition);
+    
+    $res = HelpMe::SelectMe($data, 'employeemaster');
+    
+    echo "<pre>";
+    print_r($res);
+    echo "</pre>";*/
+    
+    /******************************INSERT QUERY*******************************/    
+
+    /*$data['employeename'] = 'Ram ji';
+    $data['employee_mail_id'] = 'c@ymail.com';
+    $data['password'] = 'Dashrath ji';
+    
+    $Condition = "employee_id = '698' ";
+
+    echo HelpMe::InsertMe($data, 'employeemaster');*/
+    
+    /******************************UPDATE QUERY*******************************/
+    
+    /*$data['employeename'] = 'Ram ji';
+    $data['employee_mail_id'] = 'c@ymail.com';
+    $data['password'] = 'Dashrath ji';
+    
+    $Condition = "employee_id = '698' ";
+    //echo $exe = HelpMe::UpdateMe($data, 'employeemaster', $Condition);*/
+    
+?>
+
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
